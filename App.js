cc32bc7f8a2169258589eb3cfde2baa7959e64f2
Brillante Wang <brillante.wang@gmail.com>
2017-12-09 15:49:23 -0800

    initialize expo and update expo loading page to show me your moves

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Marth is a nerd</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#210c56',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 30,
+    color: "#fff",
+  }
+});
