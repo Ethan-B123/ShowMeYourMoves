3fa2929786c86b5d80c55ab812fcb540e9c0b236
Brillante Wang <brillante.wang@gmail.com>
2017-12-11 11:17:18 -0800

    add login page component skeleton

---
+++ frontend/components/LoginPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+class LoginPage extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  
+}
