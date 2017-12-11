ddcbac6a8e1322d2f446c6b39b6f4b8a1ff2d6f2
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-12-11 08:36:05 -0800

    add basic markers to map component

--- frontend/components/map.js
+++ frontend/components/map.js
@@ -1,6 +1,14 @@
 import React, { Component } from "react";
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import MapView from "react-native-maps";
+// import { MapView } from "Expo";
+
+const DEMO_PLAYERS = [
+  { latitude: 37.798323, longitude: -122.4209297 },
+  { latitude: 37.7973596, longitude: -122.4119088 },
+  { latitude: 37.7929689, longitude: -122.4127009 },
+  { latitude: 37.7844654, longitude: -122.4091938 }
+]
 
 class Map extends Component {
 
@@ -12,11 +20,12 @@ class Map extends Component {
   getInitialState() {
     return {
       region: {
-        latitude: 37.78825,
-        longitude: -122.4324,
+        latitude: 37.7989666,
+        longitude: -122.40135179999999,
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421,
       },
+      selectedIcon: -1
     };
   }
 
@@ -24,13 +33,30 @@ class Map extends Component {
     this.setState({ region });
   }
 
+  showPlayer(latLng, id) {
+    const icon = id === this.state.selectedIcon
+      ? require("../../assets/map_icons/person_icon_highlighted.png")
+      : require("../../assets/map_icons/person_icon_purple.png")
+    return (
+      <MapView.Marker
+        style={styles.marker}
+        key={id}
+        coordinate={latLng}
+        image={icon}
+        resizeMode="contain"
+      >
+      </ MapView.Marker>
+    )
+  }
+
   render() {
     return (
       <MapView
         style={styles.map}
         region={this.state.region}
-        onRegionChange={this.onRegionChange.bind(this)}
-      />
+        onRegionChange={this.onRegionChange.bind(this)}>
+        {DEMO_PLAYERS.map((latLng, idx) => this.showPlayer(latLng, idx))}
+      </ MapView>
     );
   }
 }
@@ -39,6 +65,12 @@ const styles = StyleSheet.create({
   map: {
     flexGrow: 1,
     alignSelf: 'stretch'
+  },
+  marker: {
+    flex: 0,
+    width: 100,
+    height: 100,
+    opacity: 0.5,
   }
 });
 
