19842ac9dd3e9781a34387122dc1d041b3e0e470
Ethan Bjornsen <ethan.bjornsen@gmail.com>
2017-12-11 18:02:50 -0800

    add styling for markers

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginPage from './frontend/components/login_page.js';
+import Map from "./frontend/components/map";
 
 export default class App extends React.Component {
   render() {
