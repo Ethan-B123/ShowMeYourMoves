0fcec9516dde3efec484e70b06c6354405250a1b
Brillante Wang <brillante.wang@gmail.com>
2017-12-13 20:33:28 -0800

    add background image to regular sign up page

--- frontend/components/regular_signup.js
+++ frontend/components/regular_signup.js
@@ -14,39 +14,58 @@ class RegularLoginPage extends React.Component {
 
   render() {
     return (
-      <KeyboardAvoidingView behavior="padding" style={styles.container}>
-        <TextInput
-          onChangeText={(val) => this.setState({email: val})}
-          placeholder="Email"
-          returnKeyType="next"
-          onSubmitEditing={() => this.passwordInput.focus()}
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoCorrect="false"
-          style={styles.input}/>
-        <TextInput
-          onChangeText={(val) => this.setState({password: val})}
-          placeholder="Password"
-          secureTextEntry
-          returnKeyType="go"
-          keyboardType="email-address"
-          autoCapitalize="none"
-          autoCorrect="false"
-          ref={(input) => this.passwordInput = input}
-          style={styles.input}/>
-        <TouchableOpacity
-          style={styles.buttonContainer}
-          onPress={() => this.props.register(this.state)}>
-          <Text style={styles.buttonText}>SIGN UP</Text>
-        </TouchableOpacity>
-      </KeyboardAvoidingView>
+      <View style={styles.container}>
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%'
+          }}
+          >
+            <Image
+              style={{flex: 1, resizeMode: 'stretch'}}
+              source={{uri: 'https://res.cloudinary.com/lara-cloud1/image/upload/v1513193501/background_image_jopmxv.png'}}
+            />
+          </View>
+          <KeyboardAvoidingView behavior="padding" style={styles.form}>
+            <TextInput
+              onChangeText={(val) => this.setState({email: val})}
+              placeholder="Email"
+              returnKeyType="next"
+              onSubmitEditing={() => this.passwordInput.focus()}
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              style={styles.input}/>
+            <TextInput
+              onChangeText={(val) => this.setState({password: val})}
+              placeholder="Password"
+              secureTextEntry
+              returnKeyType="go"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              autoCorrect={false}
+              ref={(input) => this.passwordInput = input}
+              style={styles.input}/>
+              <TouchableOpacity
+                style={styles.buttonContainer}
+                onPress={() => this.props.register(this.state)}>
+                <Text style={styles.buttonText}>SIGN UP</Text>
+              </TouchableOpacity>
+            </KeyboardAvoidingView>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20
+    flex: 1
+  },
+  form: {
+    padding: 20,
   },
   input: {
     height: 40,
