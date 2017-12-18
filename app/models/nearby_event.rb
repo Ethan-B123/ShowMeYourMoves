52f8032d70d4cf0782083244c243faa208a1d088
Brillante Wang <brillante.wang@gmail.com>
2017-12-17 19:57:43 -0800

    change nearby players and events to all players and events for the sake of the simulator

--- app/models/nearby_event.rb
+++ app/models/nearby_event.rb
@@ -3,11 +3,12 @@ class NearbyEvent < ApplicationRecord
   validates :lat, :lng, :date, :start_time, :end_time, presence: true
 
   def self.all_near_spot(lat_lng_hash, range)
-    NearbyEvent.where(
-      lat: ((lat_lng_hash[:lat] - range/2)..(lat_lng_hash[:lat] + range/2))
-    ).where(
-      lng: ((lat_lng_hash[:lng] - range/2)..(lat_lng_hash[:lng] + range/2))
-    )
+    NearbyEvent.all
+    # NearbyEvent.where(
+    #   lat: ((lat_lng_hash[:lat] - range/2)..(lat_lng_hash[:lat] + range/2))
+    # ).where(
+    #   lng: ((lat_lng_hash[:lng] - range/2)..(lat_lng_hash[:lng] + range/2))
+    # ) COMMENT BACK IN AFTER SIMULATOR. THIS IS BECAUSE IT ORIGINATES IN NY.
   end
 
   belongs_to :user
--- app/models/nearby_player.rb
+++ app/models/nearby_player.rb
@@ -3,11 +3,12 @@ class NearbyPlayer < ApplicationRecord
   validates :lat, :lng, presence: true
 
   def self.all_near_spot(lat_lng_hash, range)
-    NearbyPlayer.where(
-      lat: ((lat_lng_hash[:lat] - range/2)..(lat_lng_hash[:lat] + range/2))
-    ).where(
-      lng: ((lat_lng_hash[:lng] - range/2)..(lat_lng_hash[:lng] + range/2))
-    )
+    NearbyPlayer.all
+    # NearbyPlayer.where(
+    #   lat: ((lat_lng_hash[:lat] - range/2)..(lat_lng_hash[:lat] + range/2))
+    # ).where(
+    #   lng: ((lat_lng_hash[:lng] - range/2)..(lat_lng_hash[:lng] + range/2))
+    # ) COMMENT BACK IN. THIS IS JUST FOR SIMULATOR BECAUSE IT ORIGINATES IN NEW YORK.
   end
 
   belongs_to :user
